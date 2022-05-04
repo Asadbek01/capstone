3c11ed42637a124a33dcd445e43628121a32787f
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-05-04 19:42:41 +0200

    done

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,43 @@
-import app from './app.js'
-import connectDatabase from './config/database.js'
-import listEndpoints from "express-list-endpoints"
-import  dotenv from'dotenv';
-import  cloudinary from'cloudinary'
+import app from "./app.js";
+import connectDatabase from "./config/database.js";
+import listEndpoints from "express-list-endpoints";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 
 // Handle Uncaught exceptions
-process.on('uncaughtException', err => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down due to uncaught exception');
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
 
 // Setting up config file
 // if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
-dotenv.config({ path: 'backend/config/config.env' })
-
+dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
 // Setting up cloudinary configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
-    console.table(listEndpoints(server))
-})
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+  console.table(listEndpoints(server));
+});
 
 // Handle Unhandled Promise rejections
-process.on('unhandledRejection', err => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down the server due to Unhandled Promise rejection');
-    server.close(() => {
-        process.exit(1)
-    })
-})
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
