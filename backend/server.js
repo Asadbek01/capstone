c071016aa9180b6582c00288c1c4efe1a1456d6a
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-05-06 19:31:49 +0200

    done

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,3 @@ const server = app.listen(process.env.PORT, () => {
 });
 
 // Handle Unhandled Promise rejections
-process.on("unhandledRejection", (err) => {
-  console.log(`ERROR: ${err.stack}`);
-  console.log("Shutting down the server due to Unhandled Promise rejection");
-  server.close(() => {
-    process.exit(1);
-  });
-});
