ca258aeaceb74d5a94318dfd0f8f7fac45d5b7c5
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-05-16 05:37:33 +0200

    done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,41 +1,42 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import  {
-    registerUser,
-    loginUser,
-    forgotPassword,
-    resetPassword,
-    getUserProfile,
-    updatePassword,
-    updateProfile,
-    logout,
-    allUsers,
-    getUserDetails,
-    updateUser,
-    deleteUser
+import {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+  updatePassword,
+  updateProfile,
+  logout,
+  allUsers,
+  getUserDetails,
+  updateUser,
+  deleteUser,
+} from "../controllers/authController.js";
 
-} from'../controllers/authController.js';
+import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
-import  { isAuthenticatedUser, authorizeRoles } from '../middlewares/auth.js'
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
+router.route("/logout").get(logout);
 
-router.route('/password/forgot').post(forgotPassword)
-router.route('/password/reset/:token').put(resetPassword)
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
-router.route('/logout').get(logout);
-
-router.route('/me').get(isAuthenticatedUser, getUserProfile)
-router.route('/password/update').put(isAuthenticatedUser, updatePassword)
-router.route('/me/update').put(isAuthenticatedUser, updateProfile)
-
-router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
-router.route('/admin/user/:id')
-    .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
-    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
-    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser)
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 export default router;
